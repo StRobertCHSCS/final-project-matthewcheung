104f86da166917fd02065707e73d0c9cb445f379
robuntu <robuntu@stro.ycdsb.ca>
2019-06-07 11:02:17 -0400

    jumping is broken

--- final project.py
+++ final project.py
@@ -38,6 +38,7 @@ def on_update(delta_time):
 
     jumped()
     platforms()
+    end_goal()
 
 
 def platforms():
@@ -59,31 +60,31 @@ def platforms():
 
     # Platform collisions
     # Platform 3 Y collisions
-    if (480 <= player_x <= 720 and 270 <= player_y <= 300) and player_y + 1 >= 270:
+    if (530 <= player_x <= 670 and 270 <= player_y <= 300) and player_y + 1 >= 270:
         player_y = 270
-    if (480 <= player_x <= 720 and 300 <= player_y <= 330) and player_y - 1 <= 330:
+    if (530 <= player_x <= 670 and 300 <= player_y <= 330) and player_y - 1 <= 330:
         player_y = 330
         on_plat = True
 
     # Platform 3 X collisions
-    if (480 <= player_x <= 720 and 270 <= player_y <= 330) and player_x + 1 <= 480:
+    if (530 <= player_x <= 670 and 270 <= player_y <= 330) and player_x + 1 <= 480:
         player_x = 480
-    if (480 <= player_x <= 720 and 270 <= player_y <= 330) and player_x - 1 >= 720:
+    if (530 <= player_x <= 670 and 270 <= player_y <= 330) and player_x - 1 >= 720:
         player_x = 720
 
     # Platform 2 Y collisions
-    if (280 <= player_x <= 520 and 170 <= player_y <= 200) and player_y + 1 >= 170:
+    if (305 <= player_x <= 495 and 170 <= player_y <= 200) and player_y + 1 >= 170:
         player_y = 170
-    if (280 <= player_x <= 520 and 200 <= player_y <= 230) and player_y - 1 <= 230:
+    if (305 <= player_x <= 495 and 200 <= player_y <= 230) and player_y - 1 <= 230:
         player_y = 230
         on_plat = True
 
     # Platform 2 X collisions
-    if (280 <= player_x <= 520 and 170 <= player_y <= 230) and player_x + 1 <= 280:
+    if (305 <= player_x <= 495 and 170 <= player_y <= 230) and player_x + 1 <= 280:
         player_x = 280
-    if (280 <= player_x <= 520 and 170 <= player_y <= 230) and player_x - 1 >= 520:
+    if (305 <= player_x <= 495 and 170 <= player_y <= 230) and player_x - 1 >= 520:
         player_x = 520
-
+    '''
     # Platform 1 Y collisions
     if (80 <= player_x <= 320 and 70 <= player_y <= 100) and player_y + 1 >= 70:
         player_y = 70
@@ -96,13 +97,30 @@ def platforms():
         player_x = 80
     if (80 <= player_x <= 320 and 70 <= player_y <= 130) and player_x - 1 >= 320:
         player_x = 320
-
+    '''
 
 '''
 for i in range(len(amount_of_plats):
     if (left_edge <= player_x <= right_edge and bottom_plat <= player_y <= top_plat) and player_y + 1 >= bottom_plat:
         player_y = bottom_plat
 '''
+
+
+def platform_collisions(x, y):
+    global player_y, player_x, on_plat
+
+    # Platform 1 Y collisions
+    if (x <= player_x <= x + 240 and y <= player_y <= y + 30) and player_y + 1 >= y:
+        player_y = y
+    if (x <= player_x <= x + 240 and y + 30 <= player_y <= y + 60) and player_y - 1 <= y + 60:
+        player_y = y + 60
+        on_plat = True
+
+    # Platform 1 X collisions
+    if (x <= player_x <= x + 240 and y <= player_y <= y + 60) and player_x + 1 <= x:
+        player_x = x
+    if (x <= player_x <= x + 240 and y <= player_y <= y + 60) and player_x - 1 >= x + 240:
+        player_x = x + 240
 
 
 def jumped():
@@ -121,23 +139,37 @@ def jumped():
     if on_plat:
         velocity = 0
         jumping = False
-    '''
-    if not on_plat:
+
+    if not on_plat and not jumping:
+        velocity += 0.3
         velocity += gravity
         player_y -= velocity
         if velocity >= 20:
             velocity = 20
-    '''
+
+
+def end_goal():
+    if (594 <= player_x <= 656) and (305 <= player_y <= 430):
+        print("you win!")
 
 
 def on_draw():
     global player_x, player_y
     arcade.start_render()
-    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.RED)
 
-    arcade.draw_rectangle_filled(600, 300, 200, 10, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(400, 200, 200, 10, arcade.color.BLACK)
+    # Platforms
+    arcade.draw_rectangle_filled(600, 300, 100, 10, arcade.color.BLACK)
+    arcade.draw_rectangle_filled(400, 200, 150, 10, arcade.color.BLACK)
     arcade.draw_rectangle_filled(200, 100, 200, 10, arcade.color.BLACK)
+
+    platform_collisions(80, 70)
+
+    # Victory flag
+    arcade.draw_rectangle_filled(625, 355, 12, 100, arcade.color.DARK_BROWN)
+    arcade.draw_triangle_filled(631, 405, 631, 360, 671, 382.5, arcade.color.BANANA_YELLOW)
+
+    # Character
+    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.RED)
 
 
 def on_key_press(key, something):

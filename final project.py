fde2ddf9db928c6c288cdea15484cd219cfabd16
robuntu <robuntu@stro.ycdsb.ca>
2019-06-11 11:10:41 -0400

    in a rush. moving camera to make new levels

--- final project.py
+++ final project.py
@@ -22,6 +22,11 @@ jumping = False
 
 # Endgame variables
 victory = False
+level_1 = True
+level_2 = False
+
+# Camera variables
+cam_vars = [0, 800, 0, 600]
 
 
 def on_update(delta_time):
@@ -34,11 +39,22 @@ def on_update(delta_time):
         player_x -= 5
 
     jumped()
-    platforms()
+
+    if level_1:
+        level_1_platforms()
+    if level_2:
+        cam_vars[0] += 5
+        cam_vars[1] += 5
+        if cam_vars[0] >= 1600:
+            cam_vars[0] += 0
+            cam_vars[1] += 0
+        level_2_platforms()
+
     end_goal()
+    camera()
 
 
-def platforms():
+def level_1_platforms():
     global player_x, player_y, on_plat
 
     # Stops player from leaving the screen
@@ -81,7 +97,7 @@ def platforms():
         player_x = 280
     if (305 <= player_x <= 495 and 170 <= player_y <= 230) and player_x - 1 >= 520:
         player_x = 520
-    '''
+
     # Platform 1 Y collisions
     if (80 <= player_x <= 320 and 70 <= player_y <= 100) and player_y + 1 >= 70:
         player_y = 70
@@ -94,30 +110,10 @@ def platforms():
         player_x = 80
     if (80 <= player_x <= 320 and 70 <= player_y <= 130) and player_x - 1 >= 320:
         player_x = 320
-    '''
-
-'''
-for i in range(len(amount_of_plats):
-    if (left_edge <= player_x <= right_edge and bottom_plat <= player_y <= top_plat) and player_y + 1 >= bottom_plat:
-        player_y = bottom_plat
-'''
 
 
-def platform_collisions(x, y):
-    global player_y, player_x, on_plat
-
-    # Platform 1 Y collisions
-    if (x <= player_x <= x + 240 and y <= player_y <= y + 30) and player_y + 1 >= y:
-        player_y = y
-    if (x <= player_x <= x + 240 and y + 30 <= player_y <= y + 60) and player_y - 1 <= y + 60:
-        player_y = y + 60
-        on_plat = True
-
-    # Platform 1 X collisions
-    if (x <= player_x <= x + 240 and y <= player_y <= y + 60) and player_x + 1 <= x:
-        player_x = x
-    if (x <= player_x <= x + 240 and y <= player_y <= y + 60) and player_x - 1 >= x + 240:
-        player_x = x + 240
+def level_2_platforms():
+    pass
 
 
 def jumped():
@@ -151,6 +147,15 @@ def end_goal():
         victory = True
 
 
+def camera():
+    global cam_vars
+
+    arcade.set_viewport(cam_vars[0],
+                        cam_vars[1],
+                        cam_vars[2],
+                        cam_vars[3])
+
+
 def on_draw():
     global player_x, player_y, victory
     arcade.start_render()
@@ -159,8 +164,6 @@ def on_draw():
     arcade.draw_rectangle_filled(600, 300, 100, 10, arcade.color.BLACK)
     arcade.draw_rectangle_filled(400, 200, 150, 10, arcade.color.BLACK)
     arcade.draw_rectangle_filled(200, 100, 200, 10, arcade.color.BLACK)
-
-    platform_collisions(80, 70)
 
     # Victory flag
     arcade.draw_rectangle_filled(625, 355, 12, 100, arcade.color.DARK_BROWN)
@@ -178,7 +181,6 @@ def on_draw():
         arcade.draw_text(endgame_text, 290, 390, arcade.color.YELLOW_GREEN, 20)
         arcade.draw_text(endgame_text_2, 340, 350, arcade.color.YELLOW_GREEN, 20)
         arcade.draw_text(endgame_text_3, 240, 200, arcade.color.PINK_LAVENDER, 18)
-
 
 
 def on_key_press(key, something):
@@ -203,9 +205,16 @@ def on_key_release(key, something):
         right_pressed = False
     if key == arcade.key.A:
         left_pressed = False
-'''
-def on_mouse_press()
-'''
+
+
+def on_mouse_press(x, y, something, something_2):
+    global victory, level_1, level_2
+
+    if victory:
+        if 0 <= x <= 800 and 0 <= y <= 600:
+            if level_1:
+                level_2 = True
+
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "Final Project")
@@ -216,6 +225,7 @@ def setup():
     window.on_draw = on_draw
     window.on_key_press = on_key_press
     window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
 
     arcade.run()
 
